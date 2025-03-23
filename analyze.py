df55f712f379acdfba8e640033b541ad4c03ccfb
Lukas Petr <lukas.petr.dev@gmail.com>
2025-03-23 20:56:49 +0100

    Add result of individual functions to output

--- analyze.py
+++ analyze.py
@@ -140,6 +140,19 @@ def analyze_commit(args, commit):
 
     verdict = "equal" if neq + err == 0 else "not equal"
 
+    function_results = {}
+    for function in functions:
+        if f"{function}: unknown" in output:
+            diff_type = "unk"
+        elif f"{function}: error" in output:
+            diff_type = "err"
+        elif (os.path.exists(result_path) and
+              f"{function}.diff" in os.listdir(result_path)):
+            diff_type = "neq"
+        else:
+            diff_type = "eq"
+        function_results[function] = diff_type
+
     return {
         "no_functions": len(functions),
         "eq": eq,
@@ -149,6 +162,7 @@ def analyze_commit(args, commit):
         "err": err,
         "verdict": verdict,
         "confident": all_matched,
+        "functions": function_results
     }
 
 
